a7b0ddeb8142eb23fcb39fc508c67017d58d695a
Savinu <howfeels189@gmail.com>
2024-01-23 20:09:56 +0530

    routes added ( FINALLY )

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,18 @@
 // App.jsx
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import updated components
 import React from "react";
 import Home from "./Home";
+import Login  from './pages/Login';
 
-const App = () => (
-  <div>
-    <Home />
-  </div>
-);
+function App () {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+}
 
 export default App;
