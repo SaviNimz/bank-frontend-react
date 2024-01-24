64a6ef808473a8db21fa780129757bb57a28a511
Savinu <howfeels189@gmail.com>
2024-01-24 10:13:00 +0530

    login page hald implemented

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,11 +1,36 @@
 /* eslint-disable react/prop-types */
 import { useState } from "react";
 
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
+import { auth } from "../firebase";
+
 export const Login = ({ user }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   const [isSignUpActive, setIsSignUpActive] = useState(true);
   const handleMethodChange = () => {
     setIsSignUpActive(!isSignUpActive);
   };
+
+  const handleSignUp = () => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+    // Signed up 
+      const user = userCredential.user;
+      console.log(user);
+
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log(errorCode, errorMessage);
+
+  });
+  }
+  const handleEmailChange = (event) => setEmail(event.target.value);
+  const handlePasswordChange = (event) => setPassword(event.target.value);
 
   return (
     <section>
@@ -16,13 +41,13 @@ export const Login = ({ user }) => {
           <ul>
             <li>
               <label htmlFor="email">Email</label>
-              <input type="text" id="email" />
+              <input type="text" id="email" onChange={handleEmailChange} />
             </li>
             <li>
               <label htmlFor="password">Password</label>
-              <input type="password" id="password" />
+              <input type="password" id="password" onChange={handlePasswordChange}/>
             </li>
-          </ul>
+          </ul> 
           <button type="button" onClick={handleMethodChange}>
             {isSignUpActive ? "Sign Up" : "Sign In"}
           </button>
